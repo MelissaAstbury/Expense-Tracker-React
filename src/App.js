b2c845bb64c571a79f7adb6269c683f01b151ac2
Melissa Astbury <melissajastbury@gmail.com>
2020-04-29 15:44:03 +0100

    implemented all the components needed so they are displayed together

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+
 import "./App.css";
+
+import Header from "./Components/Header";
+import Balance from "./Components/Balance";
+import IncomeExpenses from "./Components/IncomeExpenses";
+import TransactionList from "./Components/TransactionList";
+import AddTransaction from "./Components/AddTransaction";
 
 function App() {
   return (
     <div>
-      <p>Hello</p>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+        <AddTransaction />
+      </div>
     </div>
   );
 }
