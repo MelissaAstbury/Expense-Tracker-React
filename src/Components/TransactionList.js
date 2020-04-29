617590b5742b1702850c337a70c9331b073435bc
Melissa Astbury <melissajastbury@gmail.com>
2020-04-29 16:35:58 +0100

    balance is now showing depending on the transactions

--- src/Components/TransactionList.js
+++ src/Components/TransactionList.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../Context/GlobalState";
+import Transaction from "./Transaction";
 
 const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <>
       <h3>History</h3>
-      <ul id="list" className="list">
-        <li className="minus">
-          Cash <span>=$400</span>
-          <button className="delete-btn">x</button>
-        </li>
+      <ul className="list">
+        {transactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
       </ul>
     </>
   );
